618e139a9c9fba055d620e3ccbcceba252b14657
mavied23 <rezamavid3@gmail.com>
2026-04-02 11:16:31 +0700

    uji coba oop di main sama tiket

--- src/Tiket.java
+++ src/Tiket.java
@@ -1,3 +1,6 @@
-public class Tiket {
-  
+public abstract class Tiket {
+  private String namaPenumpang; // Enkapsulasi pake private biar data ga bisa di akses langsung dari luar kelas
+  protected double hargaTiket; // Protected biar bisa di akses sama kelas turunannya ( KelasEksekutif dan KelasEkonomi )
+
+
 }
