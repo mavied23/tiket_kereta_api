71526eb791fcc0523c9419fab5c657c5379b961b
mavied23 <rezamavid3@gmail.com>
2026-04-09 14:33:10 +0700

    tugas pertemuan 3

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,37 +1,69 @@
 package main;
 
-import model.KelasEkonomi;
-import model.KelasEksekutif;
-import model.Tiket;
-import service.LayananTambahan;
-
+import model.*;
+import service.*;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-  
-  public static void main (String[] args) {
-    System.out.println("--- SISTEM PENJUALAN TIKET KERETA API ---");
-    System.out.println("1. Kelas Eksekutif: Rp. 600.000 + Fasilitas");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        // Polimorfisme: Tipe data 'Tiket' tapi isinya 'KelasEksekutif'
-        Tiket t1 = new KelasEksekutif("Reza", 600000);
-        Tiket t2 = new KelasEkonomi("Mavied", 200000);
+        Map<String, Double> daftarKereta = new HashMap<>();
+        daftarKereta.put("Argo Bandung", 150000.0);
+        daftarKereta.put("Argo Jogja", 400000.0);
+        daftarKereta.put("Argo Jakarta", 250000.0);
+        daftarKereta.put("Argo Surabaya", 550000.0);
 
-        // Cetak Tiket 1
-        System.out.println("\nPenumpang: " + t1.getNama());
-        System.out.println("Total Bayar: Rp. " + t1.hitungTotalHarga());
+        System.out.println("=== Sistem Pemesanan Tiket Kereta Api ===");
+
+        System.out.print("Masukkan nama penumpang: ");
+        String nama = input.nextLine();
+
+        System.out.println("\nDaftar Kereta Tersedia:");
+        daftarKereta.forEach((kereta, harga) -> {
+            System.out.println("- " + kereta + " (Harga Dasar: Rp " + harga + ")");
+        });
+
+        System.out.println("\nKetik Nama Kereta Yang Dipilih: ");
+        String pilihanKereta = input.nextLine();
+
+
+        if (daftarKereta.containsKey(pilihanKereta)) {
+            double hargaTerpilih = daftarKereta.get(pilihanKereta);
+
+            System.out.println("Pilih Kelas:");
+            System.out.println("1. Eksekutif");
+            System.out.println("2. Ekonomi");
+            System.out.print("Masukkan pilihan (1/2): ");
+            int pilihanKelas = input.nextInt();
         
-        // Pengecekan apakah t1 punya layanan tambahan (Downcasting)
-        if (t1 instanceof LayananTambahan) {
-            LayananTambahan layanan = (LayananTambahan) t1;
-            layanan.servisMakan();
-            layanan.bagasiEkstra();
+
+        Tiket tiketAnda;
+        if (pilihanKelas == 1) {
+            tiketAnda = new KelasEksekutif(nama, hargaTerpilih);
+        } else {
+            tiketAnda = new KelasEkonomi(nama, hargaTerpilih);
         }
 
-        System.out.println("-----------------------------------------");
-        System.out.println("2. Kelas Ekonomi: Rp. 200.000 (Tanpa Fasilitas)");
-        // Cetak Tiket 2
-        System.out.println("Penumpang: " + t2.getNama());
-        System.out.println("Total Bayar: Rp. " + t2.hitungTotalHarga());
-        System.out.println("Layanan: Standar Ekonomi.");
+        System.out.println("\n=============================");
+        System.out.println("TIKET BOOKING BERHASIL DIBUAT");
+        System.out.println("===============================");
+        System.out.println("Nama Penumpang : " + tiketAnda.getNama());
+        System.out.println("Kereta : " + pilihanKereta);
+        System.out.println("Total Bayar : Rp " + tiketAnda.hitungTotalHarga());
+
+        if (tiketAnda instanceof LayananTambahan) {
+            System.out.println("--- Fasilitas Eksekutif ---");
+            ((LayananTambahan) tiketAnda).servisMakan();
+        }
+        System.out.println("=============================");
+
+        } else {
+            System.out.println("\nError Nama kereta tidak ditemukan. Coba Jalankan ulang");
+        }
+
+        input.close();
     }
 }
